4d8afdd43786e9ae466f9b4e0059d72940cca082
Justin Perdok <justin-p@users.noreply.github.com>
2020-08-15 13:22:20 +0200

    Update and rename Format-StringToReverse.psm1 to Format-StringToReverse.ps1

--- Format-StringToReverse.ps1
+++ Format-StringToReverse.ps1
@@ -1,29 +1,42 @@
 Function Format-StringToReverse {
 	<#
 	.SYNOPSIS
-        Function that can reverses a string that has a delimiter.
+            Function that can reverses a string that has a delimiter.
+	    
 	.DESCRIPTION
-        Function that can reverses a string that has a delimiter. 
-
-        It splits the string with the supplied delimiter then uses [Array]::Reverse() to reverse the string.
-        After reversing the string it joins it with the same delimiter used to split.
-        This for example can be used to reverse the order of a users DN.
+            Function that can reverses a string that has a delimiter. 
+            It splits the string with the supplied delimiter then uses [Array]::Reverse() to reverse the string.
+            After reversing the string it joins it with the same delimiter used to split.
+            This for example can be used to reverse the order of a users DN.
+	    
+        .NOTES
+            Author: Justin Perdok (@JustinPerdok), https://justin-p.me.
+            License: MIT
+	    
+	.LINK
+	     https://github.com/justin-p/PowerShell/blob/master/Format-StringToReverse.ps1
+	     
 	.PARAMETER InputString
-        A string you would like to reverse
+            A string you would like to reverse
+	   
 	.PARAMETER Delimiter
-        The charachter between words in the string 
-  .EXAMPLE
-        PS C:\> Format-StringToReverse -InputString 'This Is A String' -Delimiter ' '
+            The charachter between words in the string 
+	   
+        .EXAMPLE
+            PS C:\> Format-StringToReverse -InputString 'This Is A String' -Delimiter ' '
             String A Is This               
-  .EXAMPLE
-        PS C:\> $User = Get-ADUsers -Identity SomeUser
-        PS C:\> $User.DistinguishedName
-            CN=SomeUser,OU=Users,OU=SomeOU,OU=CustomerName,DC=ad,DC=SomeDomain,DC=tld
-        PS C:\> $UserOUDN = $(Format-StringToReverse -InputString $($User.DistinguishedName) -SplitChar ',')
-        PS C:\> $UserOUDN
-            DC=tld,DC=SomeDomain,DC=ad,OU=CustomerName,OU=SomeOU,OU=Users,CN=SomeUser
-        PS C:\> $($UserOUDN.SubString($UserOUDN.IndexOf('OU=')).split(',')[0]).replace('OU=','')
-            CustomerName
+	   
+       .EXAMPLE
+       	    PS C:\> $User = Get-ADUsers -Identity SomeUser
+            PS C:\> $User.DistinguishedName
+	       CN=SomeUser,OU=Users,OU=SomeOU,OU=CustomerName,DC=ad,DC=SomeDomain,DC=tld
+	      
+            PS C:\> $UserOUDN = $(Format-StringToReverse -InputString $($User.DistinguishedName) -SplitChar ',')
+	    PS C:\> $UserOUDN
+               DC=tld,DC=SomeDomain,DC=ad,OU=CustomerName,OU=SomeOU,OU=Users,CN=SomeUser
+           
+	    PS C:\> $($UserOUDN.SubString($UserOUDN.IndexOf('OU=')).split(',')[0]).replace('OU=','')
+               CustomerName
 	#>    
     [CmdletBinding(SupportsShouldProcess=$true)]
     Param (
